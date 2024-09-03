8171bbd19369dc60809c0ff31244ffcdf18f4b39
AbhijeetNandvikar <abhijeetvn1234@gmail.com>
2024-09-03 12:26:30 +0530

    first working template added

---
+++ app.py
@@ -0,0 +1,381 @@
+from jinja2 import Environment, FileSystemLoader
+import json
+import asyncio
+from playwright.async_api import async_playwright
+
+
+env = Environment(loader=FileSystemLoader('.'))
+template = env.get_template('index.html')
+
+mockData = {
+  "review_id": 1,
+  "review_status": "review_complete",
+  "completed_on": "05/29/2024",
+  "exported_by": "Abhijeet Nandvikar",
+  "exported_on": "08/30/2024 02:35 PM",
+  "patient_name": "Mr Helensky, Adren J",
+  "patient_id": "00000003",
+  "patient_dob": "08/03/2000 (24 Y)",
+  "patient_sex": "M",
+  "patient_insurance_plan": "Company A",
+  "patient_member_id": "826918101",
+  "disease_type": "Cancer",
+  "affected_organ": "Lung",
+  "occurrence": "New Occurrence",
+  "phase_of_care": "Treatment",
+  "review_assessment_questions": [
+    {
+      "question": "This is an example of a sample question.",
+      "answer": "While paragraphs traditionally consist of written text, they can also incorporate visual elements, such as bullet points, lists, or images. These visual aids can enhance the organization and clarity of the content, especially in instructional or explanatory writing. By combining textual and visual elements, authors can effectively convey information and engage readers in a more dynamic manner."
+    },
+      {
+      "question": "This is an example of a sample question.",
+      "answer": "While paragraphs traditionally consist of written text, they can also incorporate visual elements, such as bullet points, lists, or images. These visual aids can enhance the organization and clarity of the content, especially in instructional or explanatory writing. By combining textual and visual elements, authors can effectively convey information and engage readers in a more dynamic manner."
+    }
+  ],
+  "treatment_plans": [
+    {
+      "name": "Treatment Plan A",
+      "description": "This is a sample recommendation description as placeholder This is a sample description as placeholder This is a sample description as placeholder",
+      "recommendation_groups": [
+        {
+          "name": "Sample Group 1",
+          "description": "This is a sample group description as placeholder This is a sample description as placeholder This is a sample description as placeholder,This is a sample group description as placeholder This is a sample description as placeholder This is a sample description as placeholder.",
+          "recommendations": [
+            {
+              "name": "Adjuvant Hormone Therapy",
+              "description": "Adjuvant hormone therapy with tamoxifen is recommended for patients with hormone receptor-positive breast cancer.",
+              "reason_for_deviation": "This is a sample note for the recommendation",
+              "not_applicable": False,
+              "additional_notes": "",
+              "is_non_standard": False,
+              "code": "A0010",
+              "code_description": "some medication description"
+            },
+            {
+              "name": "Adjuvant Hormone Therapy",
+              "description": "Adjuvant hormone therapy with tamoxifen is recommended for patients with hormone receptor-positive breast cancer.",
+              "reason_for_deviation": "This is a sample note for the recommendation",
+              "not_applicable": "true",
+              "additional_notes": "",
+              "is_non_standard": False,
+              "code": "A0010",
+              "code_description": "some medication description"
+            },
+            {
+              "name": "Adjuvant Hormone Therapy",
+              "description": "Adjuvant hormone therapy with tamoxifen is recommended for patients with hormone receptor-positive breast cancer.",
+              "reason_for_deviation": "This is a sample note for the recommendation",
+              "not_applicable": False,
+              "additional_notes": "",
+              "is_non_standard": False,
+              "code": "A0010",
+              "code_description": "some medication description"
+            },
+            
+          ]
+        },
+         {
+          "name": "Sample Group 2",
+          "description": "This is a sample group description as placeholder This is a sample description as placeholder This is a sample description as placeholder,This is a sample group description as placeholder This is a sample description as placeholder This is a sample description as placeholder.",
+          "recommendations": [
+            {
+              "name": "Adjuvant Hormone Therapy",
+              "description": "Adjuvant hormone therapy with tamoxifen is recommended for patients with hormone receptor-positive breast cancer.",
+              "reason_for_deviation": "This is a sample note for the recommendation",
+              "not_applicable": False,
+              "additional_notes": "",
+              "is_non_standard": False,
+              "code": "A0010",
+              "code_description": "some medication description"
+            },
+            {
+              "name": "Adjuvant Hormone Therapy",
+              "description": "Adjuvant hormone therapy with tamoxifen is recommended for patients with hormone receptor-positive breast cancer.",
+              "reason_for_deviation": "This is a sample note for the recommendation",
+              "not_applicable": False,
+              "additional_notes": "",
+              "is_non_standard": False,
+              "code": "A0010",
+              "code_description": "some medication description"
+            },
+            {
+              "name": "Adjuvant Hormone Therapy",
+              "description": "Adjuvant hormone therapy with tamoxifen is recommended for patients with hormone receptor-positive breast cancer.",
+              "reason_for_deviation": "This is a sample note for the recommendation",
+              "not_applicable": False,
+              "additional_notes": "",
+              "is_non_standard": False,
+              "code": "A0010",
+              "code_description": "some medication description"
+            },
+            
+          ]
+        },
+      ]
+    },
+     {
+      "name": "Treatment Plan B",
+      "description": "This is a sample recommendation description as placeholder This is a sample description as placeholder This is a sample description as placeholder",
+      "recommendation_groups": [
+        {
+          "name": "Sample Group 1",
+          "description": "This is a sample group description as placeholder This is a sample description as placeholder This is a sample description as placeholder,This is a sample group description as placeholder This is a sample description as placeholder This is a sample description as placeholder.",
+          "recommendations": [
+            {
+              "name": "Adjuvant Hormone Therapy",
+              "description": "Adjuvant hormone therapy with tamoxifen is recommended for patients with hormone receptor-positive breast cancer.",
+              "reason_for_deviation": "This is a sample note for the recommendation",
+              "not_applicable": False,
+              "additional_notes": "",
+              "is_non_standard": False,
+              "code": "A0010",
+              "code_description": "some medication description"
+            },
+            {
+              "name": "Adjuvant Hormone Therapy",
+              "description": "Adjuvant hormone therapy with tamoxifen is recommended for patients with hormone receptor-positive breast cancer.",
+              "reason_for_deviation": "This is a sample note for the recommendation",
+              "not_applicable": False,
+              "additional_notes": "",
+              "is_non_standard": False,
+              "code": "A0010",
+              "code_description": "some medication description"
+            },
+            {
+              "name": "Adjuvant Hormone Therapy",
+              "description": "Adjuvant hormone therapy with tamoxifen is recommended for patients with hormone receptor-positive breast cancer.",
+              "reason_for_deviation": "This is a sample note for the recommendation",
+              "not_applicable": False,
+              "additional_notes": "",
+              "is_non_standard": False,
+              "code": "A0010",
+              "code_description": "some medication description"
+            },
+            
+          ]
+        },
+         {
+          "name": "Sample Group 1",
+          "description": "This is a sample group description as placeholder This is a sample description as placeholder This is a sample description as placeholder,This is a sample group description as placeholder This is a sample description as placeholder This is a sample description as placeholder.",
+          "recommendations": [
+            {
+              "name": "Adjuvant Hormone Therapy",
+              "description": "Adjuvant hormone therapy with tamoxifen is recommended for patients with hormone receptor-positive breast cancer.",
+              "reason_for_deviation": "This is a sample note for the recommendation",
+              "not_applicable": False,
+              "additional_notes": "",
+              "is_non_standard": False,
+              "code": "A0010",
+              "code_description": "some medication description"
+            },
+            {
+              "name": "Adjuvant Hormone Therapy",
+              "description": "Adjuvant hormone therapy with tamoxifen is recommended for patients with hormone receptor-positive breast cancer.",
+              "reason_for_deviation": "This is a sample note for the recommendation",
+              "not_applicable": False,
+              "additional_notes": "",
+              "is_non_standard": False,
+              "code": "A0010",
+              "code_description": "some medication description"
+            },
+            {
+              "name": "Adjuvant Hormone Therapy",
+              "description": "Adjuvant hormone therapy with tamoxifen is recommended for patients with hormone receptor-positive breast cancer.",
+              "reason_for_deviation": "This is a sample note for the recommendation",
+              "not_applicable": False,
+              "additional_notes": "",
+              "is_non_standard": False,
+              "code": "A0010",
+              "code_description": "some medication description"
+            },
+            
+          ]
+        },
+         {
+          "name": "Sample Group 1",
+          "description": "This is a sample group description as placeholder This is a sample description as placeholder This is a sample description as placeholder,This is a sample group description as placeholder This is a sample description as placeholder This is a sample description as placeholder.",
+          "recommendations": [
+            {
+              "name": "Adjuvant Hormone Therapy",
+              "description": "Adjuvant hormone therapy with tamoxifen is recommended for patients with hormone receptor-positive breast cancer.",
+              "reason_for_deviation": "This is a sample note for the recommendation",
+              "not_applicable": False,
+              "additional_notes": "",
+              "is_non_standard": False,
+              "code": "A0010",
+              "code_description": "some medication description"
+            },
+            {
+              "name": "Adjuvant Hormone Therapy",
+              "description": "Adjuvant hormone therapy with tamoxifen is recommended for patients with hormone receptor-positive breast cancer.",
+              "reason_for_deviation": "This is a sample note for the recommendation",
+              "not_applicable": False,
+              "additional_notes": "",
+              "is_non_standard": False,
+              "code": "A0010",
+              "code_description": "some medication description"
+            },
+            {
+              "name": "Adjuvant Hormone Therapy",
+              "description": "Adjuvant hormone therapy with tamoxifen is recommended for patients with hormone receptor-positive breast cancer.",
+              "reason_for_deviation": "This is a sample note for the recommendation",
+              "not_applicable": False,
+              "additional_notes": "",
+              "is_non_standard": False,
+              "code": "A0010",
+              "code_description": "some medication description"
+            },
+            
+          ]
+        },
+    
+      ]
+    },
+     {
+      "name": "Treatment Plan A",
+      "description": "This is a sample recommendation description as placeholder This is a sample description as placeholder This is a sample description as placeholder",
+      "recommendation_groups": [
+        {
+          "name": "Sample Group 1",
+          "description": "This is a sample group description as placeholder This is a sample description as placeholder This is a sample description as placeholder,This is a sample group description as placeholder This is a sample description as placeholder This is a sample description as placeholder.",
+          "recommendations": [
+            {
+              "name": "Adjuvant Hormone Therapy",
+              "description": "Adjuvant hormone therapy with tamoxifen is recommended for patients with hormone receptor-positive breast cancer.",
+              "reason_for_deviation": "This is a sample note for the recommendation",
+              "not_applicable": False,
+              "additional_notes": "",
+              "is_non_standard": False,
+              "code": "A0010",
+              "code_description": "some medication description"
+            },
+            {
+              "name": "Adjuvant Hormone Therapy",
+              "description": "Adjuvant hormone therapy with tamoxifen is recommended for patients with hormone receptor-positive breast cancer.",
+              "reason_for_deviation": "This is a sample note for the recommendation",
+              "not_applicable": False,
+              "additional_notes": "",
+              "is_non_standard": False,
+              "code": "A0010",
+              "code_description": "some medication description"
+            },
+            {
+              "name": "Adjuvant Hormone Therapy",
+              "description": "Adjuvant hormone therapy with tamoxifen is recommended for patients with hormone receptor-positive breast cancer.",
+              "reason_for_deviation": "This is a sample note for the recommendation",
+              "not_applicable": False,
+              "additional_notes": "",
+              "is_non_standard": False,
+              "code": "A0010",
+              "code_description": "some medication description"
+            },
+            
+          ]
+        },
+         {
+          "name": "Sample Group 1",
+          "description": "This is a sample group description as placeholder This is a sample description as placeholder This is a sample description as placeholder,This is a sample group description as placeholder This is a sample description as placeholder This is a sample description as placeholder.",
+          "recommendations": [
+            {
+              "name": "Adjuvant Hormone Therapy",
+              "description": "Adjuvant hormone therapy with tamoxifen is recommended for patients with hormone receptor-positive breast cancer.",
+              "reason_for_deviation": "This is a sample note for the recommendation",
+              "not_applicable": False,
+              "additional_notes": "",
+              "is_non_standard": False,
+              "code": "A0010",
+              "code_description": "some medication description"
+            },
+            {
+              "name": "Adjuvant Hormone Therapy",
+              "description": "Adjuvant hormone therapy with tamoxifen is recommended for patients with hormone receptor-positive breast cancer.",
+              "reason_for_deviation": "This is a sample note for the recommendation",
+              "not_applicable": False,
+              "additional_notes": "",
+              "is_non_standard": False,
+              "code": "A0010",
+              "code_description": "some medication description"
+            },
+            {
+              "name": "Adjuvant Hormone Therapy",
+              "description": "Adjuvant hormone therapy with tamoxifen is recommended for patients with hormone receptor-positive breast cancer.",
+              "reason_for_deviation": "This is a sample note for the recommendation",
+              "not_applicable": False,
+              "additional_notes": "",
+              "is_non_standard": False,
+              "code": "A0010",
+              "code_description": "some medication description"
+            },
+            
+          ]
+        },
+         {
+          "name": "Sample Group 1",
+          "description": "This is a sample group description as placeholder This is a sample description as placeholder This is a sample description as placeholder,This is a sample group description as placeholder This is a sample description as placeholder This is a sample description as placeholder.",
+          "recommendations": [
+            {
+              "name": "Adjuvant Hormone Therapy",
+              "description": "Adjuvant hormone therapy with tamoxifen is recommended for patients with hormone receptor-positive breast cancer.",
+              "reason_for_deviation": "This is a sample note for the recommendation",
+              "not_applicable": False,
+              "additional_notes": "",
+              "is_non_standard": False,
+              "code": "A0010",
+              "code_description": "some medication description"
+            },
+            {
+              "name": "Adjuvant Hormone Therapy",
+              "description": "Adjuvant hormone therapy with tamoxifen is recommended for patients with hormone receptor-positive breast cancer.",
+              "reason_for_deviation": "This is a sample note for the recommendation",
+              "not_applicable": False,
+              "additional_notes": "",
+              "is_non_standard": False,
+              "code": "A0010",
+              "code_description": "some medication description"
+            },
+            {
+              "name": "Adjuvant Hormone Therapy",
+              "description": "Adjuvant hormone therapy with tamoxifen is recommended for patients with hormone receptor-positive breast cancer.",
+              "reason_for_deviation": "This is a sample note for the recommendation",
+              "not_applicable": False,
+              "additional_notes": "",
+              "is_non_standard": False,
+              "code": "A0010",
+              "code_description": "some medication description"
+            },
+            
+          ]
+        },
+    
+      ]
+    }
+  ]
+}
+
+# Render the template with JSON data
+html_content = template.render(mockData)
+
+# Write the rendered HTML to a file (optional)
+with open('output.html', 'w') as file:
+    file.write(html_content)
+
+async def generate_pdf(html_content, output_file):
+    async with async_playwright() as p:
+        browser = await p.chromium.launch()
+        context = await browser.new_context()
+        page = await context.new_page()
+
+        # Set the HTML content for the page
+        await page.set_content(html_content)
+
+        # Generate a PDF from the page
+        await page.pdf(path=output_file, format="A4")
+
+        # Close the browser
+        await browser.close()
+
+with open("output.html", "r") as file:
+     html_content = file.read()
+
+asyncio.run(generate_pdf(html_content, "output.pdf"))
